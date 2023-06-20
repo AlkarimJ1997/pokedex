207d0254507c9a4dae104d159418b0f58bbbc44f
Alkarim Jiwa <ajiwa10@my.bcit.ca>
2023-06-20 13:50:45 -0700

    Set up firebase

---
+++ lib/firebase/config.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, collection } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyB_K6UfWj8CCg28_moRmkvXRgczxplIoGw',
+	authDomain: 'pokedex-9a2fc.firebaseapp.com',
+	projectId: 'pokedex-9a2fc',
+	storageBucket: 'pokedex-9a2fc.appspot.com',
+	messagingSenderId: '1024885366168',
+	appId: '1:1024885366168:web:1bf2863a9e8ef767a11fed',
+	measurementId: 'G-4Z94RZCRL9',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export const usersCollection = collection(db, 'users');
+export const pokemonCollection = collection(db, 'pokemonList');
