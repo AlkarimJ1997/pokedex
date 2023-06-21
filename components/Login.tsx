b1ad479c1e7aada4bfb9550fa1244a3487caa765
Alkarim Jiwa <ajiwa10@my.bcit.ca>
2023-06-21 02:52:34 -0700

    Made login button responsive

--- components/Login.tsx
+++ components/Login.tsx
@@ -16,11 +16,15 @@ const Login = () => {
 	};
 
 	return (
-		<div className='flex h-full w-full items-center justify-center'>
+		<div className='mx-auto grid h-full max-w-[85%] place-items-center'>
 			<button
 				onClick={handleLogin}
-				className='flex items-center justify-center gap-4 border-[5px] border-slate-200 bg-transparent p-4 text-2xl font-bold uppercase tracking-[0.2em] text-slate-200 transition-all duration-[400ms] ease-in-out hover:rounded-2xl hover:border-[10px] hover:border-b-green-500 hover:border-l-yellow-300 hover:border-r-blue-700 hover:border-t-red-500'>
-				<FcGoogle size={48} />
+				className='flex items-center gap-4 border-4 border-slate-200 bg-transparent p-4 text-sm font-bold uppercase tracking-[0.2em] text-slate-200 transition-all duration-[400ms] ease-in-out hover:rounded-2xl hover:border-8 hover:border-b-green-500 hover:border-l-yellow-300 hover:border-r-blue-700 hover:border-t-red-500 md:text-base lg:text-2xl'>
+				<FcGoogle
+					size={48}
+					aria-label='Sign In With Google'
+					focusable='false'
+				/>
 				Login with Google
 			</button>
 		</div>
