130cea0843bdb2272f88cf51a9741e4244564501
Alkarim Jiwa <ajiwa10@my.bcit.ca>
2023-06-21 17:21:00 -0700

    Cleaned up code

--- constants/index.ts
+++ constants/index.ts
@@ -1,14 +1,4 @@
-import { fetchImages } from '@/utils/helpers';
-
 export const API_URL = 'https://pokeapi.co/api/v2';
 export const POKEMON_URL = `${API_URL}/pokemon?limit=5000`;
 export const POKEMON_SINGLE_URL = `${API_URL}/pokemon`;
 export const POKEMON_SPECIES_URL = `${API_URL}/pokemon-species`;
-
-export const images = fetchImages(
-	require.context('../assets/pokemons/shiny', false, /\.(png|jpe?g|svg)$/)
-);
-
-export const defaultImages = fetchImages(
-	require.context('../assets/pokemons/default', false, /\.(png|jpe?g|svg)$/)
-);
